f0ef4a94a6694affb4633172eeade16cda5d659d
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-02 18:22:51 +0300

    math-module + round() -examples.

---
+++ luento2/luento2_3b.py
@@ -0,0 +1,26 @@
+import math
+
+# piin arvo saadaan helposti math-moduulin kautta
+# VIIKKOTEHTÄVISSÄ: aina kun kysytään piin arvoa, käyttäkää tätä!
+# ÄLKÄÄ KOODATKO PIIN ARVOA ITSE TYYLIIN 3.14
+print(math.pi)
+
+# ympyrän ympärysmitta, 2 * pi * säde
+# lasketaan ympärysmitta ja pyöristetään kahteen desimaaliin
+# radius = 13
+
+# muuttuja on helppo korvata lopuksi inputilla!
+radius = input("Syötä säde:\n")
+radius = float(radius)
+
+# ympyrän ympärysmitta, 2 * pi * säde
+border = 2 * math.pi * radius
+
+# pyöristetään tulos
+border = round(border, 2)
+
+# HUOM! pelkkä round() ei riitä, jos et tallenna tulosta
+# takaisin muuttujaan!, eli pelkkä:
+# round(border, 2)
+# ei riitä!
+print(f"Ympärysmitta: {border} cm")
