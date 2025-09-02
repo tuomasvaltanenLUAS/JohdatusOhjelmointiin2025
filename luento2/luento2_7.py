b1ea7d5067683a91b372df387ae482453abf21e7
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-02 19:12:33 +0300

    More datetime examples.

--- luento2/luento2_7.py
+++ luento2/luento2_7.py
@@ -5,3 +5,12 @@ from datetime import datetime
 
 today = datetime.now()
 print(today)
+
+# formaatti: pv.kk.vvvv tt:mm:ss
+# eli %d = päivä, %m = kuukausi, %Y = vuosi, %H = tunti, %M = minuutti, %S = sekunti
+# jos haluat %d ja %m edellän olevan nollan pois:
+# Windows: %#d ja %#m
+# Unix / Linux / MacOS: %-d ja %-m
+date_text = today.strftime("%d.%m.%Y %H:%M:%S")
+print(date_text)
+print(f"Päivämäärä: {date_text}")
