b796bc2d4e8e92671f0e4842656216a87ce36fbe
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-16 17:39:02 +0300

    Lecture update.

---
+++ luento3/luento3_2.py
@@ -0,0 +1,10 @@
+# testimuuttuja, voisi korvata myös inputilla
+age = 19
+
+# ehtolause, onko ikä alle 20v?
+# jos on => tulostetaan ehtolauseen sisällä oleva teksti
+# jos ei => skipataan koko ehtolauseen sisällä oleva koodi
+
+if age < 20:
+    print("Olet alle 20v.")
+
