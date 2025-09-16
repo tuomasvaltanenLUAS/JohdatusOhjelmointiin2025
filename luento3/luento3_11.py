e5b458614d0106ee2164372ab67b64284f33eee1
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-16 19:34:04 +0300

    Lecture update.

---
+++ luento3/luento3_11.py
@@ -0,0 +1,9 @@
+# ohjelman muuttujat
+age = 20
+city = "Rovaniemi"
+student = True
+
+# voidaan tehdä myös tarkkoja täsmäehtoja, jos ohjelma niin vaatii
+if age >= 18 and city == "Rovaniemi" and student == True:
+    print("Aikuinen rovaniemeläinen opiskelija.")
+
