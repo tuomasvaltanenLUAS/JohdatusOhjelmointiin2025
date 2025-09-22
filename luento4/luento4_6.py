b5f1db7d079b438bc910429f81888dcd23aaf69c
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-22 18:46:01 +0300

    Lecture update.

---
+++ luento4/luento4_6.py
@@ -0,0 +1,12 @@
+# 2. Tee yksinkertainen laskinohjelma, jossa käyttäjä voi tehdä seuraavat
+# operaatiot: +, -, * ja /. Kysy alussa käyttäjältä kaksi eri numeroa float-muodossa,
+# ja kysy lopuksi minkä operaation hän haluaa näiden lukujen välillä suorittaa.
+
+# Kun käyttäjä on valinnut numerot ja operaation, suorita valittu operaatio numeroiden välillä.
+
+# Jos käyttäjä valitsee jakolaskun, tarkista ettei käyttäjän toinen numero ole 0.
+# Jos käyttäjä kuitenkin syötti toisena numerona nollan, ilmoita käyttäjälle että
+# nollalla ei voi jakaa. Jos käyttäjä valitsee operaation mitä ei ole olemassa,
+# ilmoita käyttäjälle: "Vääränlainen operaatio"
+
+# Tulosta lopputulos pyöristettynä yhteen desimaaliin.
