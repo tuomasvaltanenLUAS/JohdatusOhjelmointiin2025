452a0d1ec9bb5ffd23506ee0fba57cc8b01757fc
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-09-22 18:26:12 +0300

    Lecture update.

---
+++ luento4/luento4_5.py
@@ -0,0 +1,13 @@
+# 1. Tee ohjelma, joka tervehtii käyttäjää annetun kellonajan perusteella.
+# Pyöristä käytettävä tuntitieto kokonaislukuun.
+# Vastaa käyttäjälle seuraavan logiikan mukaisesti:
+
+# Jos kellonaika on 05 - 11 : vastaa "Hyvää huomenta"
+# Jos kellonaika on 12 - 17 : vastaa "Hyvää päivää"
+# Jos kellonaika on 18 - 21 : vastaa "Hyvää iltaa"
+# Jos kellonaika on 22 - 04 : vastaa "Hyvää yötä"
+
+# Lisäominaisuus:
+# Jos käyttäjä ei syötä mitään kellonaikaa, käytä automaattisesti tämänhetkistä
+# kellonaikaa (esim. datetime-moduuli). Jos käyttäjä valitsi automaattisen kellonajan,
+# ilmoita käyttäjälle "Valitaan tunti automaattisesti".
