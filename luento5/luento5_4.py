e8ce5579eea64551062d01ec8f14bd116d53cd35
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-01 18:04:20 +0300

    Lecture update.

---
+++ luento5/luento5_4.py
@@ -0,0 +1,7 @@
+text = "Rovaniemi"
+
+# tekstin kääntäminen toisinpäin Pythonissa on hieman erikoinen
+# käytännössä käytetään osatekstiä, mutta "haetaan tekstiä väärään suuntaan"
+# ja sivutuotteena teksti kääntyy toisinpäin
+reversed_text = text[::-1]
+print(reversed_text)
