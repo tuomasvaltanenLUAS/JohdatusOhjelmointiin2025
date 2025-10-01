b6912d38589011340e1638a2af980c488057edc1
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-01 17:44:38 +0300

    Lecture update.

--- luento5/luento5_1.py
+++ luento5/luento5_1.py
@@ -2,3 +2,15 @@
 # myös kirjaimellisesti lainausmerkit tekstiin
 name = 'Matti "Mahtikoodari" Meikäläinen'
 print(name)
+
+# jos halutaan kirjaimellisesti lainausmerkit tekstin sekaan
+# muista \", koska muutoin Pythonin syntaksi menee sekaisin
+# name = "Matti "Mahtikoodari" Meikäläinen"
+name = "Matti \"Mahtikoodari\" Meikäläinen"
+print(name)
+
+# hipsuilla ja lainausmerkeillä ei ole muutoin datan osalta merkitystä
+name2 = "Matti"
+name3 = 'Matti'
+print(name2)
+print(name3)
---
+++ luento5/luento5_2a.py
@@ -0,0 +1,18 @@
+# alkuperäinen teksti
+text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# osateksti, ensimmäisestä merkistä kymmenenteen merkkiin
+subtext1 = text[0:10]
+print(subtext1)
+
+# osateksti, kuudennesta merkistä viidenteentoista merkkiin
+subtext2 = text[5:15]
+print(subtext2)
+
+# osateksti, viimeiset viisi merkki
+subtext3 = text[-5:]
+print(subtext3)
+
+# osateksti, kaikki teksti viidennen merkin jälkeen
+subtext4 = text[5:]
+print(subtext4)
