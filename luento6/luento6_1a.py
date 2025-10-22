17cc5ed971b4f1e7a3c1e6e4b603a1d3d0dafa93
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-22 17:32:51 +0300

    Lecture update.

--- luento6/luento6_1a.py
+++ luento6/luento6_1a.py
@@ -1,2 +1,7 @@
-# aiheena tänään TOISTOLAUSEET!
-print("Tervetuloa!")
+# for -silmukka, joka ajaa 10 kertaa koodirivin
+# käytännössä me käsketään Pythonia
+# ajamaan silmukan sisällä oleva koodi 10 kertaa
+# x on ensimmäisellä kierroksella 0
+# ja viimeisellä kierroksella 9
+for x in range(10):
+    print(f"Numero: {x}")
---
+++ luento6/luento6_1b.py
@@ -0,0 +1,15 @@
+# ks. tiedosto luento6_1a.py
+# käytännössä for-silmukka tekee
+# saman kuin tämä alla oleva koodi
+# silmukoilla voimme siis automatisoida koodia!
+
+print(f"Kierros: 0")
+print(f"Kierros: 1")
+print(f"Kierros: 2")
+print(f"Kierros: 3")
+print(f"Kierros: 4")
+print(f"Kierros: 5")
+print(f"Kierros: 6")
+print(f"Kierros: 7")
+print(f"Kierros: 8")
+print(f"Kierros: 9")
