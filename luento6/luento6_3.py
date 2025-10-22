9096cee7f0002416b8cfc0f2b07870b711372b0a
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-22 17:46:37 +0300

    Lecture update.

---
+++ luento6/luento6_3.py
@@ -0,0 +1,13 @@
+# for-silmukan laskurimuuttuja on normaali muuttuja
+# ja sitä voi käyttää myös laskutoimituksissa
+# for-silmukka huolehtii cycle-muuttujan
+# korottamisesta automaattisesti
+
+# kokeile tätä ohjelmaa myös Python Tutorissa!
+print("Tervetuloa!\n")
+
+for cycle in range(10):
+    number = cycle * 2
+    print(number)
+
+print("\nKiitos!")
