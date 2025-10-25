ebdcb92ca3aa176a27802a92c293e5bd90b2b328
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-25 11:38:42 +0300

    Lecture update.

--- luento7/luento7_1.py
+++ luento7/luento7_1.py
@@ -1,6 +1,11 @@
+# muista asentaan ensin colorama, ks. ohje Moodlessa
 from colorama import Fore, Back, Style
 
+# muutetaan tekstin väri
 print(Fore.CYAN + 'Eri väristä tekstiä!')
+print("Lisää tekstiä samalla värillä.")
+
+# muutetaan taustaväri valkoiseksi
 print(Back.LIGHTWHITE_EX + 'Eri taustavärikin!')
 
 # palautetaan kaikki normaaliksi
