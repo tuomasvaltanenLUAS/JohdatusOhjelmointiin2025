a6971e486e250b6cebeaa78844e9b0f9602a9357
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-25 12:41:33 +0300

    Lecture update.

--- luento7/luento7_3.py
+++ luento7/luento7_3.py
@@ -4,3 +4,10 @@
 # koodissa kuitenkin käytetään aina sitten PIL
 # HUOM! älä asenna PIL ikinä, vaan aina Pillow!
 from PIL import Image, ImageDraw
+
+img = Image.new('RGB', (500, 300), (128, 128, 128))
+draw = ImageDraw.Draw(img)
+
+draw.ellipse((100, 100, 150, 200), fill=(255, 0, 0), outline=(0, 0, 0))
+
+img.save("pillow_test.png")
