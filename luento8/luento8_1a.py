f555c6d42229dac9381a58ce732fb8d450966960
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-28 17:25:15 +0200

    Lecture update.

--- luento8/luento8_1a.py
+++ luento8/luento8_1a.py
@@ -1,2 +1,8 @@
-# Johdatus ohjelmointiin, luento 8, kokoelmat ja lisää toistolauseita, 28.10.2025
-print("Tervetuloa!")
+# tehdään lista tuotteista
+products = ["Pölynimuri", "Kahvinkeitin", "Jääkaappi", "Pakastin"]
+
+# tämä on ns. raakamuoto, voi käyttää debuggauksessa
+# tai jos muutoin haluaa nopeasti tietää mitä listan sisällä on
+# tätä muotoa ei näytetä yleensä käyttäjälle, koska tämä on liian raaka
+# formaatti
+# print(products)
