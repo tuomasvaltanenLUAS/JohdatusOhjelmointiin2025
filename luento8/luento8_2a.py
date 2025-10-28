2ddae02acb921e2b4f2a055fbb36b18702313fbb
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-28 17:53:27 +0200

    Lecture update.

---
+++ luento8/luento8_2a.py
@@ -0,0 +1,11 @@
+# tehdään lista tuotteista
+products = ["Pölynimuri", "Kahvinkeitin", "Jääkaappi",
+            "Pakastin", "Älypuhelin", "Kynnysmatto"]
+
+# KOKEILE MYÖS PYTHON TUTORISSA
+
+# silmukoidaan tuotelista läpi
+# ensimmäinen kierros sisältää ensimmäisen tuotteen
+# viimeinen kierros sisältää viimeisen tuotteen
+for item in products:
+    print(item)
