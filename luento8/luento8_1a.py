d5af0f79c2cec13c4f4ef74653d8473a38d573c5
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-28 17:30:30 +0200

    Lecture update.

--- luento8/luento8_1a.py
+++ luento8/luento8_1a.py
@@ -6,3 +6,11 @@ products = ["Pölynimuri", "Kahvinkeitin", "Jääkaappi", "Pakastin"]
 # tätä muotoa ei näytetä yleensä käyttäjälle, koska tämä on liian raaka
 # formaatti
 # print(products)
+
+# haetaan yksittäinen tuota ylläolevasta listasta
+# indeksi 2, eli kolmas tuote
+# myös negatiivinen indeksi on mahdollinen, esim. -1 => viimeinen tuote
+# negatiivisia indeksejä käytetään yleensä tekstien kanssa,
+# ei niinkään kokoelmien kanssa
+text = products[2]
+print(text)
