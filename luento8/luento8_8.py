4c9b1f270b9d03f300e8b453a37d8de6cb589dc6
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-10-28 19:32:13 +0200

    Lecture update.

---
+++ luento8/luento8_8.py
@@ -0,0 +1,8 @@
+# kaks eri listaa
+foods = ["leipä", "porkkana", "tomaatti"]
+drinks = ["tee", "kahvi", "mehu"]
+
+# yhdistetään kaksi listaa yhdeksi listaksi
+# HUOM! tämä ei ole sama asia kuin lista, joka koostuu listoista
+everything = foods + drinks
+print(everything)
---
+++ luento8/luento8_9.py
@@ -0,0 +1,12 @@
+# esimerkki, jossa listojen näppäryys näkyy loistavasti
+grades = [5, 8, 7, 9, 10, 7, 8, 6, 10, 9]
+
+# keskiarvon määritelmä on tämä:
+# lukujen summa / lukujen määrällä
+total = sum(grades)
+amount = len(grades)
+
+# lasketaan keskiarvo
+average = total / amount
+average = round(average, 2)
+print(average)
