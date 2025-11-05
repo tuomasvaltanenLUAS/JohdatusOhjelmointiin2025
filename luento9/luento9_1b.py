aea0c254fcd62d705f7d0074333529f5e1dc115b
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-05 17:35:45 +0200

    Lecture update.

---
+++ luento9/luento9_1b.py
@@ -0,0 +1,14 @@
+# tuotteet omissa kategorioissaan
+books = ["Da Vinci Code", "Taru sormusten herrasta", "Light Fantastic"]
+movies = ["Jurassic Park", "Interstellar", "Forrest Gump"]
+
+# kaikki verkkokaupan tuotteet ovat omina listoinaan tässä päälistassa
+products = [books, movies]
+
+# silmukoidaan kategoriat läpi
+for category in products:
+
+    # ... ja sitten silmukoidaan tämän hetkisen / aktiivisen
+    # kategorian kaikki tuotteet
+    for item in category:
+        print(item)
