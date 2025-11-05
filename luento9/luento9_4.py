c78bf2b60b01fd9a571d215c9624db3ba44135f0
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-05 18:12:12 +0200

    Lecture update.

--- luento9/luento9_4.py
+++ luento9/luento9_4.py
@@ -1,3 +1,5 @@
+import var_dump as vd
+
 # luodaan hotelli no. 1
 hotel_1 = {
     "name": "Snow Line Hotels",
@@ -20,4 +22,27 @@ hotel_2 = {
 
 # asetetaan molemmat hotellit samaan listaan
 hotels = [hotel_1, hotel_2]
-print(hotels)
+# print(hotels)
+# print()
+
+# ['name'] => str(16) "Snow Line Hotels"
+# ['rating'] => float(4.3)
+# ['wifi'] => bool(True)
+# ['free_breakfast'] => bool(True)
+# ['services'] => list(5)
+#    [0] => str(5) "sauna"
+#    [1] => str(8) "meetings"
+#    [2] => str(10) "restaurant"
+#    [3] => str(7) "parking"
+#    [4] => str(7) "safaris"
+# ['price_level'] => int(4)
+
+# var_dump auttaa tutkimaan datan yksityiskohtia helpommin
+# vd.var_dump(hotels)
+
+# first_hotel = hotels[0]
+# vd.var_dump(first_hotel)
+
+# silmukoidaan hotellidata läpi
+for hotel in hotels:
+    print(hotel['name'])
