5a8e780bd6c49e6ac872bd9979be526df6105c85
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-05 18:02:23 +0200

    Lecture update.

---
+++ luento9/luento9_4.py
@@ -0,0 +1,23 @@
+# luodaan hotelli no. 1
+hotel_1 = {
+    "name": "Snow Line Hotels",
+    "rating": 4.3,
+    "wifi": True,
+    "free_breakfast": True,
+    "services": ["sauna", "meetings", "restaurant", "parking", "safaris"],
+    "price_level": 4
+}
+
+# luodaan hotelli no. 2
+hotel_2 = {
+    "name": "North Ice Hostel",
+    "rating": 3.5,
+    "wifi": True,
+    "free_breakfast": False,
+    "services": ["sauna", "parking"],
+    "price_level": 2
+}
+
+# asetetaan molemmat hotellit samaan listaan
+hotels = [hotel_1, hotel_2]
+print(hotels)
