387df6bd99fbb1156989ca8a6614839a2d26adb9
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-26 17:40:19 +0200

    Lecture examples updated.

--- luento_11/luento11_1.py
+++ luento_11/luento11_1.py
@@ -5,5 +5,9 @@ file_handle = open("weekdays.txt", "r")
 # => tietotyyppi on oletuksena string/tekstiä
 content = file_handle.read()
 
+# tiedostoa ei tässä vaiheessa enää tarvita
+# koska tiedoston sisältö ladattiin jo ylempänä muuttujaan
+file_handle.close()
+
 # tulostetaan kuin mikä tahansa muuttuja
 print(content)
