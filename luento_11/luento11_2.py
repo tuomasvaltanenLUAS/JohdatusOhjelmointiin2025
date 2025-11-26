85131bd9da5923140204cd28cfcb7c67b9281269
Tuomas Valtanen <tuomas.valtanen@lapinamk.fi>
2025-11-26 17:49:20 +0200

    Lecture examples updated.

--- luento_11/luento11_2.py
+++ luento_11/luento11_2.py
@@ -10,14 +10,17 @@ counter = 1
 while True:
     # haetaan seuraava rivi
     line = file_handle.readline()
-    print(f"{counter} - {line}")
-
-    counter = counter + 1
 
     # jos tiedoston loppu tulee vastaan
     # eli line == None, lopetetaan silmukka
     if not line:
         break
+
+    # tulostetaan tekstitiedoston rivi + laskuriarvo
+    print(f"{counter} - {line}")
+
+    # korotetaan laskuria ennen seuraavaa riviä
+    counter = counter + 1
 
 
 # tiedostoa ei tässä vaiheessa enää tarvita
